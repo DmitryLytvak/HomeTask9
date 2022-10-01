952ab6c6378579a307c77a0aa139449189d5e74c
Litvak <kefirodua@gmail.com>
2022-10-01 15:56:57 +0300

    test

--- src/ua/ithillel/java/Controller/FileLogger.java
+++ src/ua/ithillel/java/Controller/FileLogger.java
@@ -1,7 +1,10 @@
 package ua.ithillel.java.Controller;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import ua.ithillel.java.model.FileLoggerConfiguration;
+import ua.ithillel.java.model.FileMaxSizeReachedException;
 import ua.ithillel.java.model.LoggingLevel;
 
 public class FileLogger {
@@ -12,17 +15,33 @@ public class FileLogger {
 		this.fileLoggerConfiguration = fileLoggerConfiguration;
 	}
 	
-	public void	debug(String message) {
+	public void debug(String message) {
 		String level = fileLoggerConfiguration.getLevel();
 		if (LoggingLevel.DEBUG.toString().equalsIgnoreCase(level)) {
-			String fileBody = "[" + LocalDateTime.now() + "]: [" + level + "]: Message - " + message;
-
+			try {
+				toFile(message);
+			} catch (FileMaxSizeReachedException e) {
+				System.out.println(e);
+			}
 		}
 	}
 	
+	
 	public void info(String message) {
-		String fileBody = "[" + LocalDateTime.now() + "]: ["
-				+ fileLoggerConfiguration.getLevel() + "]: Message - " + message;
+		try {
+			toFile(message);
+		} catch (FileMaxSizeReachedException e) {
+			System.out.println(e);
+		}
 	}
-
+	
+	private void toFile(String message) {
+		try (FileWriter writer = new FileWriter("log.txt", true)) {
+			String fileBody = "[" + LocalDateTime.now() + "]: ["
+					+ fileLoggerConfiguration.getLevel() + "]: Message - " + message;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 }
